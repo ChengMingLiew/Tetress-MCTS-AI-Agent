9bf343c08ae72aabf4717ea6787738c0fa5ea44e
ChengMingLiew <125233730+ChengMingLiew@users.noreply.github.com>
2025-08-04 11:43:22 +0800

    agent playing random moves

---
+++ rand_agent/program.py
@@ -0,0 +1,55 @@
+# COMP30024 Artificial Intelligence, Semester 1 2024
+# Project Part B: Game Playing Agent
+
+from referee.game import PlayerColor, Action, PlaceAction
+
+from .selfboard import*
+
+
+class Agent:
+    """
+    This class is the "entry point" for your agent, providing an interface to
+    respond to various Tetress game events.
+    """
+
+    def __init__(self, color: PlayerColor, **referee: dict):
+        """
+        This constructor method runs when the referee instantiates the agent.
+        Any setup and/or precomputation should be done here.
+        """
+        self._color = color
+
+        match color:
+            case PlayerColor.RED:
+                print("Testing: I am playing as RED")
+            case PlayerColor.BLUE:
+                print("Testing: I am playing as BLUE")
+
+        # Board initiation
+        self.board = Selfboard()
+
+    def action(self, **referee: dict) -> Action:
+        """
+        This method is called by the referee each time it is the agent's turn
+        to take an action. It must always return an action object. 
+        """
+
+        return find_random_action(self.board, self._color)
+
+    def update(self, color: PlayerColor, action: Action, **referee: dict):
+        """
+        This method is called by the referee after an agent has taken their
+        turn. You should use it to update the agent's internal game state. 
+        """
+
+        # There is only one action type, PlaceAction
+        place_action: PlaceAction = action
+        c1, c2, c3, c4 = place_action.coords
+
+        self.board.apply_action(action)
+
+        if color == self._color:
+            print(f"Testing: {color} played PLACE action: {c1}, {c2}, {c3}, {c4}")
+
+
+    
